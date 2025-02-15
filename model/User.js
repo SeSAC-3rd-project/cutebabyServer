85310b6f7eb9175c92c2ee8e7ef74303a62c95d4
Yundo-git <ydh7804@gmail.com>
2025-02-16 01:03:34 +0900

    User.js수정

--- model/User.js
+++ model/User.js
@@ -1,11 +1,12 @@
-import db from '../config/db.js';  // MySQL 연결 객체
+import db from "../config/db.js"; // MySQL 연결 객체
 
 // 이메일로 사용자 정보 조회
-export const getUserByEmail = (email) => {
-  console.log('model에서 이메일 ',email)
+export const getUserByEmail = (inputEmail) => {
+  console.log("model에서 이메일 ", inputEmail);
   return new Promise((resolve, reject) => {
-    const query = 'SELECT * FROM userinfo WHERE userid = ?';  // 이메일을 기준으로 사용자 조회
-    db.query(query, [email], (err, result) => {  // 쿼리 실행
+    const query = "SELECT * FROM userinfo WHERE userid = ?"; // 이메일을 기준으로 사용자 조회
+    db.query(query, [inputEmail], (err, result) => {
+      // 쿼리 실행
       if (err) reject(err);
       else resolve(result); // 결과 반환
     });
